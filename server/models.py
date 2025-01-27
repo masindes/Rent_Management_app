0ea039424191a7d54c15a9939eed22bcc08319e6
masinde sylvester <masinde.sylvester@yahoo.com>
2025-01-27 15:26:47 +0300

    initial payment_table

--- server/models.py
+++ server/models.py
@@ -25,3 +25,15 @@ class Tenant(db.Model):
 
     def __repr__(self):
         return f'<Tenant {self.name} , Phone:{self.phone}, Unit_id:{self.unit_id}, Email:{self.email}>'
+    
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    payment_type = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    payment_date = db.Column(db.DateTime, nullable=False)
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenant.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Payment {self.payment_type} , Status:{self.status}, Amount:{self.amount}, Payment_date:{self.payment_date}>'
+
